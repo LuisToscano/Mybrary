4146d64c1e4ac58702aa7c1c20dcc513f8c3b95e
Luis Toscano <luisgtoscano@gmail.com>
2021-09-06 05:36:30 -0500

    lesson 2

---
+++ models/author.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const autorSchema = new mongoose.Schema({
+    name: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Author', autorSchema);
